fb444fe6f8d4a73ec38fe4061ec84dcc6d63fdbd
VugarVA <vamirov@bu.edu>
2024-06-13 05:35:14 +0400

    Edits

---
+++ Care Buddy/TermsConditions.swift
@@ -0,0 +1,18 @@
+//
+//  TermsConditions.swift
+//  Care Buddy
+//
+//  Created by Vugar Amirov on 6/12/24.
+//
+
+import SwiftUI
+
+struct TermsConditions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TermsConditions()
+}
